876e6d8c7309655f0af4ec107df477644107545f
Soo Philip Kim <philipjkim@gmail.com>
2012-06-09 00:12:38 +0900

    add TODO list to home page; modified invalid css files

--- app.rb
+++ app.rb
@@ -8,7 +8,7 @@ class Note
   include DataMapper::Resource
   property :id, Serial
   property :content, Text, :required => true
-  property :complete, Text, :required => true, :default => false
+  property :complete, Boolean, :required => true, :default => false
   property :created_at, DateTime
   property :updated_at, DateTime
 end
