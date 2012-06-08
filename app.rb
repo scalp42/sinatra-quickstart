35fbe2d1065a9a430f6d62eeb56c6e4a1caaf4bc
Soo Philip Kim <philipjkim@gmail.com>
2012-06-08 22:59:01 +0900

    added database/table configurations

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require 'sinatra/base'
+require 'datamapper'
+
+DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/todo.db")
+
+class Note
+  include DataMapper::Resource
+  property :id, Serial
+  property :content, Text, :required => true
+  property :complete, Text, :required => true, :default => false
+  property :created_at, DateTime
+  property :updated_at, DateTime
+end
+
+DataMapper.finalize.auto_upgrade!
 
 class App < Sinatra::Base
   get '/' do
